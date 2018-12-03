8aade242fa104b7639422940c4d888c370fb5983
mchausse <maxime.chausse@hotmail.ca>
2018-12-03 17:09:37 -0500

    Creation du projet

---
+++ TP2/TP2/modele/Programmeur.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP2.modele{
+    public class Programmeur{
+        public String Courriel { get; set; }
+    }
+}
