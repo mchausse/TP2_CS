585808b7682241e7c56cdb412e3072c2c59f60f7
mchausse <maxime.chausse@hotmail.ca>
2018-12-03 17:21:51 -0500

    Creation de la classe Programmeur

--- TP2/TP2/modele/Programmeur.cs
+++ TP2/TP2/modele/Programmeur.cs
@@ -4,7 +4,20 @@ using System.Linq;
 using System.Web;
 
 namespace TP2.modele{
-    public class Programmeur{
-        public String Courriel { get; set; }
+    public class Programmeur {
+        public string Courriel { get; set; }
+        public string Nom { get; set; }
+        public string Equipes { get; set; }
+
+        public Programmeur() { }
+        public Programmeur(string c, string n){
+            Courriel = c;
+            Nom = n;
+        }
+        public Programmeur(string c, string n, string e) {
+            Courriel = c;
+            Nom = n;
+            Equipes = e;
+        }
     }
 }
